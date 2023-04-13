330198a651c61c00e422ac065bc23c0c727f2dc4
Gicks4Fun <130671084+Gicks4Fun@users.noreply.github.com>
2023-04-13 14:34:06 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,7 @@
 import streamlit
 
 streamlit.title('My Parents New Health Diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach')
+streamlit.text('Hard-Boiled Free-Range Egg')
