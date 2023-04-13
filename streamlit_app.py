b3bf33a92dd7fd4fe2c704058fc1f1a867b01623
Gicks4Fun <130671084+Gicks4Fun@users.noreply.github.com>
2023-04-13 15:33:20 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,16 +1,11 @@
 
 import streamlit
 import pandas
-streamlit.title('My Parents New Health Diner')
+streamlit.title('My Moms New Health Diner')
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗Kale, Spinach')
 streamlit.text('🐔Hard-Boiled Free-Range Egg')
-streamlit.header('🥑Breakfast Menu')
-streamlit.text('🍞Omega 3 & Blueberry Oatmeal')
-streamlit.text('🍞Kale, Spinach')
-streamlit.text('🍞Hard-Boiled Free-Range Egg')
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 #my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 #streamlit.dataframe(my_fruit_list)
